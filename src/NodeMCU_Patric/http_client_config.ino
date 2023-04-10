82f99f7e91cb67c0f249cf32975e74df218400c3
swat121 <stark2542@gmail.com>
2023-04-10 15:04:46 +0300

    Update: http_client_config

--- src/NodeMCU_Patric/http_client_config.ino
+++ src/NodeMCU_Patric/http_client_config.ino
@@ -1,18 +1,41 @@
 void checkConnectToServer() {
   String whiteIp = parts[0] + "." + parts[1] + "." + parts[2] + ".";
+  String payload;
   int timeout = 700;
   int port = 8080;
   String uri = "/ping";
+  String uri2 = "/clients";
   char host[100];
+  char karenPOST[100];
   for (int i = 1; i < 255; i++) {
     sprintf(host, "http://%s%d:%d%s", whiteIp.c_str(), i, port, uri.c_str());
     Serial.println(host);
     if (ping(host, timeout) == "pong") {
       Serial.print("Karen host is: ");
       Serial.println(whiteIp + i);
+      sprintf(karenPOST, "http://%s%d:%d%s", whiteIp.c_str(), i, port, uri2.c_str());
+      Serial.println(karenPOST);
+      payload = "name=Patric&ip=" + WiFi.localIP().toString();
+      String r = POSTRequest(karenPOST, "application/x-www-form-urlencoded", payload);
+      Serial.println("POST request to Karen");
+      Serial.println(r);
       break;
     }
   }
+}
+
+String POSTRequest(char link[], String contentType, String payload) {
+  String response = "";
+  HTTPClient http;
+  WiFiClient wifiClient;
+  http.begin(wifiClient, link);
+  http.addHeader("Content-Type", contentType);
+  int httpCode = http.POST(payload);
+  if (httpCode > 0) {
+    response = http.getString();
+  }
+  http.end();
+  return response;
 }
 
 void splitString(String ip_address) {
